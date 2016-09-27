7b597d1f49d070d1f000989f0004e169dab1c153
Bikramjeet S Sandhu <bss46@afsconnect1.njit.edu>
2016-09-27 13:57:55 -0400

    Adding files

---
+++ SignupPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1> Bikram Sandhu's Sign-Up Page</h1>
+
+<form action="action_page.php">
+First Name:<br>
+<input type="text" name="firstname"><br>
+Last Name:<br>
+<input type="text" name="lastname"><br>
+Date of Birth:<br>
+<input type="date" name="bday"><br>
+Email Address:<br>
+<input type="text" name="email"><br>
+Phone Number:<br>
+<input type="tel" name="phone"><br>
+Password:<br>
+<input type="password" name="password"><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
